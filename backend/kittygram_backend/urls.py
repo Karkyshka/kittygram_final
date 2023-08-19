7177ffc1e08d5a2b1185b2ef215ec07bded2c52b
Karkyshka <karkyshakarka@yandex.ru>
2023-08-19 15:10:39 +0300

    flake8

--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -1,6 +1,6 @@
 from cats.views import AchievementViewSet, CatViewSet
-from django.conf import settings
-from django.conf.urls.static import static
+# from django.conf import settings
+# from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
